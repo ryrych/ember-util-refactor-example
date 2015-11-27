72752a42f2d9fce439508fe28003d5713fc9009b
Wojtek Ryrych <wojtek.ryrych@gmail.com>
2015-11-27 21:40:34 +0100

    Refactored errorize

--- app/utils/errorize.js
+++ app/utils/errorize.js
@@ -6,38 +6,31 @@ var errorize = Ember.Object.extend({
     this.set('errorsList', {});
   },
 
-  passedErrorObject() {
-    return !Ember.isNone(this.get('errors'));
+  validObject() {
+    return !!this.get('errors');
   },
 
-  buildErrorObjectFor(error) {
-    let field = this.getErrorObjectField(error);
-    let message = this.getErrorObjectMessage(error);
-    this.addOrCreateErrorForField(field, message);
+  buildErrorMessage(error) {
+    let field = this.getErrorField(error);
+    let message = error.detail;
+    this.addErrorMessage(field, message);
   },
 
-  getErrorObjectField(error) {
+  getErrorField(error) {
     // { source: { pointer:'/data/attributes/email' } }
     return (/\w+$/.exec(error.source.pointer))[0].camelize();
   },
 
-  getErrorObjectMessage(error) {
-    return error.detail;
-  },
-
-  addOrCreateErrorForField(field, message) {
-    if (Ember.isNone(this.get('errorsList')[field])) {
-      this.get('errorsList')[field] = [{message}];
-    } else {
-      this.get('errorsList')[field].push({message});
-    }
+  addErrorMessage(field, message) {
+      (this.get('errorsList')[field] = this.get('errorsList')[field] || [])
+      .push({message});
   },
 
   serialize() {
-    if (!this.passedErrorObject()) {
+    if (!this.validObject()) {
       return {};
     }
-    this.get('errors.responseJSON.errors').forEach(error => this.buildErrorObjectFor(error));
+    this.get('errors.responseJSON.errors').forEach(error => this.buildErrorMessage(error));
     return this.get('errorsList');
   },
 });
